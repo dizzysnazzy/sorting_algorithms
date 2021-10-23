877725ea4c6e72833afd175a955d26d775fdec6e
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-10-23 13:47:44 -0700

    insertion sort file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,66 @@
+/*
+ * File: 1-insertion_sort_list.c
+ * Auth: Dismas Kipchumba
+ */
+
+#include "sort.h"
+
+/**
+ * len_list - returns the length of a linked list
+ * @p: pointer to the list
+ *
+ * Return: length of list
+ */
+int len_list(listint_t *p)
+{
+  int len = 0;
+
+  while (p)
+    {
+      len++;
+      p = p->next;
+    }
+  return (len);
+}
+
+/**
+ * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
+ * @list: double pointer to the list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+  listint_t *curr = NULL, *one = NULL;
+  listint_t *two = NULL, *three = NULL, *four = NULL;
+
+  if (!list || !(*list) || len_list(*list) < 2)
+    return;
+
+  curr = *list;
+
+  while (curr)
+    {
+      if (curr->prev && curr->n < curr->prev->n)
+	{
+	  one = curr->prev->prev;
+	  two = curr->prev;
+	  three = curr;
+	  four = curr->next;
+
+	  two->next = four;
+	  if (four)
+	    four->prev = two;
+	  three->next = two;
+	  three->prev = one;
+	  if (one)
+	    one->next = three;
+	  else
+	    *list = three;
+	  two->prev = three;
+	  curr = *list;
+	  print_list(*list);
+	  continue;
+	}
+      else
+	curr = curr->next;
+    }
+}
