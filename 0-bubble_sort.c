b339f66f1bbe8b284ec85c3d6641d2c0c7afd068
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-10-25 03:09:27 -0700

    bubble and insertion file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,23 +13,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-  unsigned int i, j;
-  int swp;
+unsigned int i, j;
+int swp;
 
-  if (size < 2)
-    return;
+if (size < 2)
+return;
 
-  for (i = 0; i < size; i++)
-    {
-      for (j = 0; j < size - i - 1; j++)
-	{
-	  if (array[j] > array[j + 1])
-	    {
-	      swp = array[j];
-	      array[j] = array[j + 1];
-	      array[j + 1] = swp;
-	      print_array(array, size);
-	    }
-	}
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+swp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = swp;
+print_array(array, size);
+}
+}
+}
 }
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,14 +13,14 @@
  */
 int len_list(listint_t *p)
 {
-  int len = 0;
+int len = 0;
 
-  while (p)
-    {
-      len++;
-      p = p->next;
-    }
-  return (len);
+while (p)
+{
+len++;
+p = p->next;
+}
+return (len);
 }
 
 /**
@@ -29,38 +29,38 @@ int len_list(listint_t *p)
  */
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *curr = NULL, *one = NULL;
-  listint_t *two = NULL, *three = NULL, *four = NULL;
+listint_t *curr = NULL, *one = NULL;
+listint_t *two = NULL, *three = NULL, *four = NULL;
 
-  if (!list || !(*list) || len_list(*list) < 2)
-    return;
+if (!list || !(*list) || len_list(*list) < 2)
+return;
 
-  curr = *list;
+curr = *list;
 
-  while (curr)
-    {
-      if (curr->prev && curr->n < curr->prev->n)
-	{
-	  one = curr->prev->prev;
-	  two = curr->prev;
-	  three = curr;
-	  four = curr->next;
+while (curr)
+{
+if (curr->prev && curr->n < curr->prev->n)
+{
+one = curr->prev->prev;
+two = curr->prev;
+three = curr;
+four = curr->next;
 
-	  two->next = four;
-	  if (four)
-	    four->prev = two;
-	  three->next = two;
-	  three->prev = one;
-	  if (one)
-	    one->next = three;
-	  else
-	    *list = three;
-	  two->prev = three;
-	  curr = *list;
-	  print_list(*list);
-	  continue;
-	}
-      else
-	curr = curr->next;
-    }
+two->next = four;
+if (four)
+four->prev = two;
+three->next = two;
+three->prev = one;
+if (one)
+one->next = three;
+else
+*list = three;
+two->prev = three;
+curr = *list;
+print_list(*list);
+continue;
+}
+else
+curr = curr->next;
+}
 }
