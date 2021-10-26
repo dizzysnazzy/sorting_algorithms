72dc93b25a2183c65d6ccea325f01402dd812732
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-10-26 13:16:32 -0700

    header file

--- deck.h
+++ deck.h
@@ -12,12 +12,12 @@
  * @DIAMOND: diamond
  */
 typedef enum kind_e
-  {
-   SPADE = 0,
-   HEART,
-   CLUB,
-   DIAMOND
-  } kind_t;
+{
+SPADE = 0,
+HEART,
+CLUB,
+DIAMOND
+} kind_t;
 
 /**
  * struct card_s - Playing card
@@ -28,8 +28,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-  const char *value;
-  const kind_t kind;
+const char *value;
+const kind_t kind;
 } card_t;
 
 /**
@@ -41,9 +41,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-  const card_t *card;
-  struct deck_node_s *prev;
-  struct deck_node_s *next;
+const card_t *card;
+struct deck_node_s *prev;
+struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
