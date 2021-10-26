71e321ecb09f0093ee0b3b2c65de0fe7b97dc6c0
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-10-26 00:51:47 -0700

    shell sort file

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * shell_sort - function sorts an array of integers using
+ * the shell sort algorithm with Knuth sequence
+ * @array: the array of integers
+ * @size: size of the array
+ *
+ * Return: none
+ */
+void shell_sort(int *array, size_t size)
+{
+size_t space = 0, j, i;
+int tmp;
+
+if (size < 2)
+return;
+
+while ((space = space * 3 + 1) < size)
+;
+
+space = (space - 1) / 3;
+
+for (; space > 0; space = (space - 1) / 3)
+{
+for (i = space; i < size; i++)
+{
+tmp = array[i];
+for (j = i; j >= space && tmp <= array[j - space]; j -= space)
+array[j] = array[j - space];
+array[j] = tmp;
+}
+print_array(array, size);
+}
+}
--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ int len_list(listint_t *p);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
